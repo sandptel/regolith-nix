465e28bc593f5447fdb3e6e41c9df65209eb8aa9
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-05-10 21:32:39 +0530

    Add files via upload

---
+++ ilia.nix
@@ -0,0 +1,74 @@
+
+{
+  pkg-config,
+  libgee,
+  ninja,
+  gtk-layer-shell,
+  pkgs,
+  lib,
+  stdenv,
+  makeWrapper,
+  fetchgit,
+  json-glib,
+  gettext,
+  gobject-introspection,
+  intltool,
+  gtk3,
+  tracker,
+  meson,
+  vala,
+  cmake,
+}:
+
+stdenv.mkDerivation {
+  pname = "ilia";
+  version = "3.1";
+  
+  src = pkgs.fetchFromGitHub {
+    owner = "regolith-linux";
+    repo = "ilia";
+    rev = "r3_1-ubuntu-jammy";
+    hash = "sha256-4MKVwaepLOaxHFSwiks5InDbKt+B/Q2c97mM5yHz4eU=";
+  };
+
+
+  nativeBuildInputs = [
+
+  ];
+
+  buildInputs = [
+    makeWrapper
+    json-glib
+    gettext
+    gobject-introspection
+    intltool
+    gtk3
+    tracker
+    meson
+    vala
+    cmake
+    pkg-config
+    libgee
+    ninja
+    gtk-layer-shell
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp src/ilia $out/bin
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    mkdir -p $out/share/glib-2.0/schemas/
+    glib-compile-schemas --targetdir=$out/share/glib-2.0/schemas $src/data
+    wrapProgram "$out/bin/ilia" --set XDG_DATA_DIRS "$out/share/gsettings-schemas/ilia-3.1"
+  '';
+
+  meta = {
+    mainProgram = "ilia";
+    description = "A GTK-based Desktop Executor";
+    homepage = "https://github.com/regolith-linux/ilia";
+    license = lib.licenses.gpl3Plus;
+  };
+}
