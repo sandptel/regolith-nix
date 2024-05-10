ea4728b2427ca458c292b9449c78e55d0d3c3efe
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-05-10 21:38:58 +0530

    Updated sha256 regolith-inputd.nix

--- regolith-inputd.nix
+++ regolith-inputd.nix
@@ -8,7 +8,7 @@ src = fetchFromGitHub {
 owner = "regolith-linux";
 repo = "regolith-inputd";
 rev = "r3_1";
-hash = "sha256-wwdAY00Ju/q6S4LEucRZ27krFIdOevq/SL4VMRqmwnk=";
+hash = "sha256-PX9lWeAfJ79zEhXEKFTryf780JcoY7k6XRSxzHM1WWw=";
 };
 
 cargoHash = "sha256-q+s/XDQtHy86NBRc9EwmnCldEF4FLM599kSwAwlxgY8=";
