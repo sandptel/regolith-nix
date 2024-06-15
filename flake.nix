402967b726b18a0467c695c373ab215419a9dd55
sandptel <sandeeppatel626735@gmail.com>
2024-06-16 04:16:14 +0530

    Added a dev shell
    Added a dev shell instead of multiple packages cause packages shows the follow error ```❯ nix flake show
    warning: Git tree '/home/roronoa/Documents/flakeadd/regolith-nix' is dirty
    git+file:///home/roronoa/Documents/flakeadd/regolith-nix
    └───packages
    error: 'packages' is not an attribute set
    ```

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Packaging Regolith Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+  system= "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell{
+      nativeBuildInputs =[
+        (import ./ilia/default.nix {inherit pkgs;})
+        (import ./i3xrocks/default.nix {inherit pkgs;})
+        (import ./remontoire/default.nix {inherit pkgs;})
+        (import ./regolith-powerd/default.nix {inherit pkgs;})
+        (import ./regolith-inputd/default.nix {inherit pkgs;})
+        (import ./regolith-displayd/default.nix {inherit pkgs;})
+        (import ./rofication/default.nix {inherit pkgs;})
+      ];
+    };
+
+    #since only one package can stay in one flake file will need to add them indivisually
+    # packages.${system}.ilia= import ./ilia/default.nix{inherit pkgs;};
+};
+}
