4344629226cf2bbbfa2be41d75d78a272bfeb5f5
sandptel <sandeeppatel626735@gmail.com>
2024-08-03 23:20:16 +0530

    added packages

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = {self, nixpkgs }: 
   let 
   system= "x86_64-linux";
   pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    nixosModules."regolith" = import ./packages.nix;
+    nixosModules.regolith = import ./packages.nix;
     
     devShells.${system}.default = pkgs.mkShell{
       nativeBuildInputs =[
--- packages.nix
+++ packages.nix
@@ -1,1 +1,31 @@
-{ config, ... }: { options = {}; config = {}; }
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.programs.regolith;
+in {
+  options.programs.regolith = {
+    enable = mkEnableOption "Add regolith packages";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.hello;
+      defaultText = literalExpression "pkgs.hello";
+      description = "Add extra packages too add";
+    };
+
+    };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      (import ./ilia/default.nix {inherit pkgs;})
+      (import ./i3xrocks/default.nix {inherit pkgs;})
+        (import ./remontoire/default.nix {inherit pkgs;})
+        (import ./regolith-powerd/default.nix {inherit pkgs;})
+        (import ./regolith-inputd/default.nix {inherit pkgs;})
+        (import ./regolith-displayd/default.nix {inherit pkgs;})
+        (import ./rofication/default.nix {inherit pkgs;})
+        cfg.package
+  ];
+    };
+  }
