bd4d9e2ef03a27cc90e6728ddbcedc5dc31e7b6e
sandptel <sandeeppatel626735@gmail.com>
2025-02-11 11:39:20 +0530

    added: trawl at 0.2.5

---
+++ packages/regolith-look-extra.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "regolith-look-extra";
+  version = "0.9.2";
+
+  src = fetchFromGitHub {
+    owner = "regolith-linux";
+    repo = "regolith-look-extra";
+    rev = "v${version}";
+    hash = "sha256-DHou7BB8mem/CkGs4YEYbxQ95HRMUxGw1E59U9U9gTo=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/usr/share/regolith-look
+    cp -r $src/usr $out
+  
+    # substituteInPlace $out/usr/share/regolith-look/*/*/ \
+    # --replace-quiet /usr /run/current-system/sw/usr \
+  '';
+
+  meta = {
+    description = "Additional Looks for the Regolith Desktop";
+    homepage = "https://github.com/regolith-linux/regolith-look-extra";
+    license = lib.licenses.gpl3Plus; 
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "regolith-look-extra";
+    platforms = lib.platforms.all;
+  };
+}
